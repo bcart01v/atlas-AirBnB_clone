c7201dcf5a83acfd59f70b5cff220e1136acf27b
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-04 14:04:46 -0600

    Added file storage class, adds the ability to. Well. Store files.

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 
 
 import uuid
+import models
 from datetime import datetime
 
 class BaseModel:
@@ -23,12 +24,14 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-    
+            models.storage.new(self)
+ 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     
     def save(self):
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
         """Returns a dictionary containing all keys/values of __dict__ of the instance."""
---
+++ models/engine/file_storage.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""Module for FileStorage class."""
+
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """Class for serializing and deserializing instances to and from JSON file."""
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to JSON file """
+        obj_dict = {obj_id: obj.to_dict() for obj_id, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, "w") as file:
+            json.dump(obj_dict, file)
+
+    def reload(self):
+        """Deserialization of the json file to __objects """
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                obj_dict = json.load(f)
+            for obj_id, obj in obj_dict.items():
+                cls_name = obj["__class__"]
+                if cls_name == 'BaseModel':
+                    obj = BaseModel(**obj)
+                    FileStorage.__objects[obj_id] = obj
+        except FileNotFoundError:
+            pass
