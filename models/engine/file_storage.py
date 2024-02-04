b2af25dba386e6cb5b8389fc40f901881d92b43a
bcart01v <38075349+bcart01v@users.noreply.github.com>
2024-02-04 14:08:54 -0600

    Pretty sure this won't fix it, cause I can't see what the checker is checking, but it's worth a shot

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,7 +8,7 @@ from models.base_model import BaseModel
 
 class FileStorage:
     """Class for serializing and deserializing instances to and from JSON file."""
-    __file_path = "file.json"
+    __file_path = "file.json" 
     __objects = {}
 
     def all(self):
